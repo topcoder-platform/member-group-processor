7ee5a39ec5b252fa732393cd1cc38eea2d0f8432
Dushyant Bhalgami <dushyantbhalgami@gmail.com>
2019-09-20 11:04:11 +0530

    updated for v5 groups api

--- src/app.js
+++ src/app.js
@@ -2,79 +2,92 @@
  * The application entry point
  */
 
-global.Promise = require('bluebird')
-const _ = require('lodash')
-const config = require('config')
-const logger = require('./common/logger')
-const Kafka = require('no-kafka')
-const co = require('co')
-const ProcessorService = require('./services/ProcessorService')
-const healthcheck = require('topcoder-healthcheck-dropin')
+global.Promise = require('bluebird');
+const _ = require('lodash');
+const config = require('config');
+const logger = require('./common/logger');
+const Kafka = require('no-kafka');
+const co = require('co');
+const ProcessorService = require('./services/ProcessorService');
+const healthcheck = require('topcoder-healthcheck-dropin');
 
 // create consumer
-const options = { connectionString: config.KAFKA_URL, groupId: config.KAFKA_GROUP_ID }
+const options = { connectionString: config.KAFKA_URL, groupId: config.KAFKA_GROUP_ID };
 if (config.KAFKA_CLIENT_CERT && config.KAFKA_CLIENT_CERT_KEY) {
-  options.ssl = { cert: config.KAFKA_CLIENT_CERT, key: config.KAFKA_CLIENT_CERT_KEY }
+  options.ssl = { cert: config.KAFKA_CLIENT_CERT, key: config.KAFKA_CLIENT_CERT_KEY };
 }
-const consumer = new Kafka.GroupConsumer(options)
+const consumer = new Kafka.GroupConsumer(options);
 
 // data handler
-const dataHandler = (messageSet, topic, partition) => Promise.each(messageSet, (m) => {
-  const message = m.message.value.toString('utf8')
-  logger.info(`Handle Kafka event message; Topic: ${topic}; Partition: ${partition}; Offset: ${
-    m.offset}; Message: ${message}.`)
-  let messageJSON
-  try {
-    messageJSON = JSON.parse(message)
-  } catch (e) {
-    logger.error('Invalid message JSON.')
-    logger.error(e)
-    // ignore the message
-    return
-  }
-  const payload = _.get(messageJSON, 'payload', {})
-  if (_.get(payload, 'traitId', '').toLowerCase() !== 'communities') {
-    logger.info('The message traitId field is not \'communities\'. ignoring.')
-    // ignore the message
-    return
-  }
-  return co(function * () {
-    yield ProcessorService.processMessage(payload)
-  })
-    // commit offset
-    .then(() => consumer.commitOffset({ topic, partition, offset: m.offset }))
-    .then(() => logger.debug('Successfully processed message'))
-    .catch((err) => logger.error(err))
-})
+const dataHandler = (messageSet, topic, partition) =>
+  Promise.each(messageSet, m => {
+    const message = m.message.value.toString('utf8');
+    logger.info(
+      `Handle Kafka event message; Topic: ${topic}; Partition: ${partition}; Offset: ${m.offset}; Message: ${message}.`
+    );
+    let messageJSON;
+    try {
+      messageJSON = JSON.parse(message);
+    } catch (e) {
+      logger.error('Invalid message JSON.');
+      logger.error(e);
+      // ignore the message
+      return;
+    }
+    const payload = _.get(messageJSON, 'payload', {});
+    if (_.get(payload, 'traitId', '').toLowerCase() !== 'communities') {
+      logger.info("The message traitId field is not 'communities'. ignoring.");
+      // ignore the message
+      return;
+    }
+    return (
+      co(function*() {
+        if (topic === 'identity.notification.create') {
+          yield ProcessorService.addMemberToClosedCommunity(payload)
+        } else {
+          yield ProcessorService.processMessage(payload);
+        }
+      })
+        // commit offset
+        .then(() => logger.debug('Successfully processed message'))
+        .catch(err => logger.error(err))
+        .finally(() => {
+          logger.debug('Commiting Offset');
+          consumer.commitOffset({ topic, partition, offset: m.offset });
+        })
+    );
+  });
 
 // check if there is kafka connection alive
-function check () {
+function check() {
   if (!consumer.client.initialBrokers && !consumer.client.initialBrokers.length) {
-    return false
+    return false;
   }
-  let connected = true
+  let connected = true;
   consumer.client.initialBrokers.forEach(conn => {
-    logger.debug(`url ${conn.server()} - connected=${conn.connected}`)
-    connected = conn.connected & connected
-  })
-  return connected
+    logger.debug(`url ${conn.server()} - connected=${conn.connected}`);
+    connected = conn.connected & connected;
+  });
+  return connected;
 }
 
 consumer
-  .init([{
-    subscriptions: config.KAFKA_TOPICS,
-    handler: dataHandler
-  }])
+  .init([
+    {
+      subscriptions: config.KAFKA_TOPICS,
+      handler: dataHandler,
+    },
+  ])
   // consume configured topics
   .then(() => {
-    logger.info('Initialized.......')
-    healthcheck.init([check])
-    logger.info('Adding topics successfully.......')
-    logger.info(config.KAFKA_TOPICS)
-    logger.info('Kick Start.......')
+    logger.info('Initialized.......');
+    healthcheck.init([check]);
+    logger.info('Adding topics successfully.......');
+    logger.info(config.KAFKA_TOPICS);
+    logger.info('Kick Start.......');
   })
-  .catch((err) => logger.error(err))
+  .catch(err => logger.error(err));
 
 if (process.env.NODE_ENV === 'test') {
-  module.exports = consumer
+  module.exports = consumer;
 }
--- src/services/ProcessorService.js
+++ src/services/ProcessorService.js
@@ -1,104 +1,163 @@
 /**
  * Service for community processor.
  */
-const _ = require('lodash')
-const Joi = require('joi')
-const logger = require('../common/logger')
-const helper = require('../common/helper')
-const config = require('config')
-const axios = require('axios')
+const _ = require('lodash');
+const Joi = require('joi');
+const logger = require('../common/logger');
+const helper = require('../common/helper');
+const config = require('config');
+const axios = require('axios');
 
 /**
  * Process Kafka message payload
  * @param {Object} message the message payload
  */
-function * processMessage (message) {
+function* processMessage(message) {
   // find out communities and flags
-  const communities = []
-  const flags = []
-  const traits = _.get(message, 'traits.data', [])
-  _.each(traits, (item) => {
+  const communities = [];
+  const flags = [];
+  const traits = _.get(message, 'traits.data', []);
+  _.each(traits, item => {
     _.forIn(item, (value, key) => {
-      const c = key.toLowerCase()
+      const c = key.toLowerCase();
       if (_.indexOf(communities, c) < 0) {
-        communities.push(c)
-        flags.push(value)
+        if (value != null) {
+          communities.push(c);
+          flags.push(value);
+        }
       }
-    })
-  })
+    });
+  });
 
   // get groups
-  const token = yield helper.getM2Mtoken()
+  const token = yield helper.getM2Mtoken();
   const tcAPIClient = axios.create({
     baseURL: config.TC_API_BASE_URL,
-    timeout: 10000,
+    timeout: 1000000,
     headers: {
       Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    }
-  })
-  logger.info('Get groups data.')
-  const groupsRes = yield tcAPIClient.get('/v3/groups')
-  const groups = groupsRes.data
+      'Content-Type': 'application/json',
+    },
+  });
 
   // handle each community
-  const memberId = message.userId
+  const memberId = message.userId;
+
+  // get group members
+  logger.info(`Get groups of member ${memberId}`);
+  const memberGroupsRes = yield tcAPIClient.get(`/v5/groups/?memberId=${memberId}&membershipType=user`);
+  const memberGroups = memberGroupsRes.data;
+
   for (let i = 0; i < communities.length; i += 1) {
-    const community = communities[i]
-    const flag = flags[i]
+    const community = communities[i];
+    const flag = flags[i];
+    console.log(`${community} = ${flag}`);
+
     // find corresponding group id
-    const group = _.find(groups.result.content, (g) => g.name && g.name.toLowerCase() === community)
-    if (!group) {
-      logger.error(`Invalid community: ${community}`)
-      continue
-    }
-    const groupId = group.id
-    // get group members
-    logger.info(`Get memberships of group ${groupId}`)
-    const groupMembersRes = yield tcAPIClient.get(`/v3/groups/${groupId}/members`)
-    const groupMembers = groupMembersRes.data
+    const groupsRes = yield tcAPIClient.get(`/v5/groups?name=${community}`);
+    const group = groupsRes.data[0];
+    const groupId = group.oldId;
+
     // find user from group members
-    const foundMembership = _.find(groupMembers.result.content, (m) => m.membershipType === 'user' && m.memberId === memberId)
+    const foundMembership = _.find(memberGroups, g => g.oldId === groupId);
     if (flag) {
-      // add user if user is not in the group
       if (!foundMembership) {
-        logger.info(`Add user ${memberId} to group ${groupId}`)
-        yield tcAPIClient.post(`/v3/groups/${groupId}/members`, { param: { memberId, membershipType: 'user' } })
+        logger.info(`Add user ${memberId} to group ${groupId}`);
+        yield tcAPIClient.post(`/v5/groups/${groupId}/members`, {
+          param: { memberId: memberId.toString(), membershipType: 'user' },
+        });
       } else {
-        logger.info(`The user ${memberId} is already in group ${groupId}`)
+        logger.info(`The user ${memberId} is already in group ${groupId}`);
       }
     } else {
-      // remove user if user is in the group
-      if (foundMembership) {
-        logger.info(`Remove user ${memberId} from group ${groupId}`)
-        yield tcAPIClient.delete(`/v3/groups/${groupId}/members/${foundMembership.id}`)
-      } else {
-        logger.info(`The user ${memberId} is already NOT in group ${groupId}`)
+      if (flag != null && foundMembership) {
+        logger.info(`Remove user ${memberId} from group ${groupId}`);
+        yield tcAPIClient.delete(`/v5/groups/${groupId}/members/${memberId}`);
       }
     }
   }
 }
 
 processMessage.schema = {
-  message: Joi.object().keys({
-    userId: Joi.number().integer().min(1).required(),
-    userHandle: Joi.string(),
-    traitId: Joi.string().required(),
-    categoryName: Joi.string().required(),
-    createdBy: Joi.number().integer().min(1).required(),
-    createdAt: Joi.string().required(),
-    updatedBy: Joi.number().integer().min(1),
-    updatedAt: Joi.string(),
-    traits: Joi.object().keys({
-      traitId: Joi.string(),
-      data: Joi.array().items(Joi.object())
-    }).required()
-  }).required()
+  message: Joi.object()
+    .keys({
+      userId: Joi.number()
+        .integer()
+        .min(1)
+        .required(),
+      userHandle: Joi.string(),
+      traitId: Joi.string().required(),
+      categoryName: Joi.string().required(),
+      createdBy: Joi.number()
+        .integer()
+        .min(1)
+        .required(),
+      createdAt: Joi.string().required(),
+      updatedBy: Joi.number()
+        .integer()
+        .min(1),
+      updatedAt: Joi.string(),
+      traits: Joi.object()
+        .keys({
+          traitId: Joi.string(),
+          data: Joi.array().items(Joi.object()),
+        })
+        .required(),
+    })
+    .required(),
+};
+
+/**
+ * Process `identity creation` messages and add members to the closed community|groups
+ * @param {Object} message the message payload
+ */
+function* addMemberToClosedCommunity(message) {
+  // get groups
+  const token = yield helper.getM2Mtoken();
+  const tcAPIClient = axios.create({
+    baseURL: config.TC_API_BASE_URL,
+    timeout: 1000000,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const provider = _.get(message, 'profile.provider', undefined);
+  if (provider) {
+    switch (provider) {
+      case 'wipro-adfs':
+        const groupsRes = yield tcAPIClient.get(`/v5/groups?ssoId=${provider}`);
+        const group = groupsRes.data[0];
+        const groupId = group.oldId;
+
+        logger.info(`Add user ${message.id} to group ${groupId}`);
+        yield tcAPIClient.post(`/v5/groups/${groupId}/members`, {
+          param: { memberId: message.id.toString(), membershipType: 'user' },
+        });
+
+        break;
+      case 'Zurich':
+        const groupsRes = yield tcAPIClient.get(`/v5/groups?ssoId=${provider}`);
+        const group = groupsRes.data[0];
+        const groupId = group.oldId;
+
+        logger.info(`Add user ${message.id} to group ${groupId}`);
+        yield tcAPIClient.post(`/v5/groups/${groupId}/members`, {
+          param: { memberId: message.id.toString(), membershipType: 'user' },
+        });
+
+        break;
+      default:
+        logger.info('Skipping message as no ssoId found');
+    }
+  }
 }
 
 // Exports
 module.exports = {
-  processMessage
-}
+  processMessage,
+  addMemberToClosedCommunity,
+};
 
-logger.buildService(module.exports)
+logger.buildService(module.exports);
